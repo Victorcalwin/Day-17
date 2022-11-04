434fe6d5291b87f6d541f91e1afc74b11cfc4b0d
Victorcalwin <104540827+Victorcalwin@users.noreply.github.com>
2022-11-04 12:43:44 +0530

    Add files via upload

---
+++ Day 17  Independent Day/script.js
@@ -0,0 +1,36 @@
+ let result = function(){
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "10";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "9";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "8";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "7";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "6";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "5";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "4";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "3";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "2";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "1";
+	setTimeout(()=>{
+		document.getElementById('num').innerHTML = "Happy Independace day";
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+	},1000)
+}
+result();
